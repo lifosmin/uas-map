b7e502d4fc785365d6d445f75e68db174d2deb61
Ploeroe <raphael.hakim@student.umn.ac.id>
2022-12-06 21:56:56 +0700

    third time downgrading

--- uas-server/routes/api.php
+++ uas-server/routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::prefix('token')->group(function () {
+        Route::post('/logout', [UserController::class, 'logout']);
+        Route::post('/refresh', [UserController::class, 'refreshToken']);
+    });
+    
+    Route::prefix('user')->group(function () {
+        Route::get('/', [UserController::class, 'getUser']);
+    });
+    
 });
+
+Route::prefix('user')->group(function () {
+    Route::post('/signin', [UserController::class, 'signIn']);
+    Route::post('/signup',[UserController::class, 'signUp']);
+});
+
